97656eb16984d9fdfda89aefde2933754019537a
sebaLitter26 <sebastian_litter@hotmail.com>
2020-03-18 02:37:38 -0300

    second commit

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -11,35 +11,30 @@ import { Character, Film } from '../models/index.models';
 import { mergeMap, map, switchMap, tap, concatAll   } from 'rxjs/operators';
 import { Observable, of , forkJoin, Subscription } from 'rxjs';
 
-import {animate, state, style, transition, trigger} from '@angular/animations';
-
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+
 })
 export class charactersComponent implements OnInit, OnDestroy {
 
   private character$Subscription: Subscription;
+  private films$Subscription: Subscription;
+
   private character$: Observable<any[]> ;
   characters: Character[] ;
   film$: any ;
   film: Film ;
   showFilms: any[] = [];
 
-  displayedColumns: string[] = ['name', 'eye_color', 'gender','films'];
+  displayedColumns: string[] = ['name', 'eye_color', 'gender', 'films'];
   dataSource: MatTableDataSource<Character>;
   expandedElement: any | null;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild('table', { static: true }) table;
 
   constructor(
                 private apiService: ApiService,
@@ -51,37 +46,41 @@ export class charactersComponent implements OnInit, OnDestroy {
     this.film$ = this.activatedRoute.snapshot.params;
     const people:string[] = this.activatedRoute.snapshot.params.characters.split(',') ;
 
-    let peopleAux: any[] =[];
+    const peopleAux: any[] =[];
     this.character$Subscription = this.getArrayRequest(people).subscribe((results:any) => {
       peopleAux.push(results);
     },
-    (error)=>console.log("onError", error),
+    (error) => console.log('onError', error),
     () => {
-      //this.characters = peopleAux;
-      this.dataSource = new MatTableDataSource(peopleAux);
+      this.characters = peopleAux;
+      peopleAux.forEach((character, pos)=>{
+        this.expandFilmDetails(character, pos);
+      });
+      console.log(this.characters);
+      this.dataSource = new MatTableDataSource(this.characters);
+
+      this.table.renderRows();
       this.dataSource.paginator = this.paginator;
-      console.log("onComplete", this.dataSource);
     }
     );
 
   }
 
-  expandFilmDetails(character){
+  expandFilmDetails(character, pos){
 
-    let filmsAux: any[] =[];
-    this.character$Subscription = this.getArrayRequest(character.films).subscribe((results:any) => {
+    const filmsAux: any[] =[];
+    this.films$Subscription = this.getArrayRequest(character.films).subscribe((results:any) => {
       filmsAux.push(results);
     },
     (error)=>console.log("onError", error),
     () => {
-      //this.characters = peopleAux;
 
-      console.log("onComplete", filmsAux);
-      this.showFilms = filmsAux;
+      this.characters[pos].films = filmsAux;
     }
     );
 
   }
+
 
 
   getArrayRequest(arrayRequestString: string[]): Observable<any> {
@@ -102,7 +101,7 @@ export class charactersComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.character$Subscription.unsubscribe();
-   // this.film$.unsubscribe();
+    this.films$Subscription.unsubscribe();
   }
 
 
